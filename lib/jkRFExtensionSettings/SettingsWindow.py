6876a300f675d6fedb1cc99878af4d4671c8b79f
Jens Kutilek <webmail@kutilek.de>
2015-12-15 22:25:25 +0100

    Add option to save settings on edit; order elements by addition

--- lib/jkRFExtensionSettings/SettingsWindow.py
+++ lib/jkRFExtensionSettings/SettingsWindow.py
@@ -4,9 +4,11 @@ import vanilla
 from Setting import Setting
 
 class SettingsWindow(BaseWindowController):
-    def __init__(self, extension_id, name):
+    def __init__(self, extension_id, name, save_on_edit=False):
         self._extension_id = extension_id
         self._name = name
+        self._save_on_edit = save_on_edit
+        self.settings_list = []
         self.settings = {}
         
         self.column = 80
@@ -14,9 +16,11 @@ class SettingsWindow(BaseWindowController):
         self.height = 200
     
     def add(self, settings_key, default_value, display_name=None):
+        self.settings_list.append(settings_key)
         self.settings[settings_key] = Setting(settings_key, default_value, display_name)
     
     def _init_settings(self):
+        self.settings_list = []
         self.settings = {}
     
     def _load_settings(self):
@@ -38,14 +42,15 @@ class SettingsWindow(BaseWindowController):
         self.height = 20 + 26 * len(self.settings)
         self.w = vanilla.Window((self.width, self.height), self._name)
         i = 0
-        for setting in self.settings.itervalues():
+        for key in self.settings_list:
+            setting = self.settings[key]
             ui_objects = []
             if type(setting.default_value) == bool:
                 checkbox = vanilla.CheckBox(
                         (self.column + 10, 10 + 26 * i, -10, 22),
                         setting.name,
                         value = setting.value,
-                        #callback = self._edit_value,
+                        callback = self._edit_value,
                 )
                 setting.ui_object = checkbox
                 ui_objects.append(checkbox)
@@ -53,7 +58,7 @@ class SettingsWindow(BaseWindowController):
                 slider = vanilla.Slider(
                         (self.column + 10, 10 + 26 * i, -10, 22),
                         value = setting.value,
-                        #callback = self._edit_value,
+                        callback = self._edit_value,
                 )
                 setting.ui_object = slider
                 ui_objects.append(slider)
@@ -67,8 +72,13 @@ class SettingsWindow(BaseWindowController):
                 setattr(self.w, "%s_%i" % (setting.key, j), ui_objects[j])
             i += 1
     
+    def _edit_value(self):
+        if self._save_on_edit:
+            self._save_settings()
+    
     def windowCloseCallback(self, sender):
         self._save_settings()
+        super(SettingsWindow, self).windowCloseCallback(sender)
     
     def show(self):
         self._load_settings()
@@ -79,7 +89,7 @@ class SettingsWindow(BaseWindowController):
 
 
 def test():
-    my_settings = SettingsWindow("de.kutilek.test", "My Settings")
+    my_settings = SettingsWindow("de.kutilek.test", "My Settings", True)
     my_settings.add("mySlider", 0.0, "My Slider")
     my_settings.add("myCheckbox", True)
     my_settings.show()
